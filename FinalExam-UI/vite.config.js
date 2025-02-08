4bfc693b73719ed8b61dfe994ed702f9876cbe6c
Darius Zilionis <Darius.Zilionis@aol.com>
2025-02-08 23:54:17 +0200

    quickfix

--- FinalExam-UI/vite.config.js
+++ FinalExam-UI/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/final-exam-task",
-})
+  base: '/final-exam-task/FinalExam-UI',
+});
